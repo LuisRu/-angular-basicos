91ef6f806cc18efd3458a547a79e6974c4a91926
Luis Rubido <luisrubidoloureiro@hotmail.com>
2022-05-18 10:18:11 +0200

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroeModule } from './heroe/heroe.modulo';
+import { ContadorModule } from './contador/contador.modulo';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroeModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
